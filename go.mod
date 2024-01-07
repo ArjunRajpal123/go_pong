a293e945548c13285ea4319549a8129fca5ba578
ArjunRajpal123 <81884929+ArjunRajpal123@users.noreply.github.com>
2024-01-07 00:44:10 -0800

    updated webapp - starting

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/arjunrajpal123/go_pong
 
 go 1.21.5
 
-require github.com/a-h/templ v0.2.513 // indirect
+require github.com/a-h/templ v0.2.513
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	component := hello("World")
+	component := hello("World") // Use the correct package name when calling the function
 	http.Handle("/", templ.Handler(component))
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
